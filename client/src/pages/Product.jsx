1905ad53788207388a064b6105b8a58f9d22768c
cropacoulus <desmar217@gmail.com>
2022-03-15 22:40:58 +0700

    shopping cart with redux toolkit

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -8,6 +8,8 @@ import { mobile } from "../responsive";
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { publicRequest } from "../requestMethods";
+import { addProduct } from "../redux/cartRedux";
+import { useDispatch } from "react-redux";
 
 const Container = styled.div``;
 
@@ -123,6 +125,9 @@ const Product = () => {
   const id = (location.pathname.split("/")[2])
   const [product, setProduct] = useState({})
   const [quantity, setQuantity] = useState(1)
+  const [color, setColor] = useState("")
+  const [size, setSize] = useState("")
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const getProduct = async () => {
@@ -144,6 +149,12 @@ const Product = () => {
     }
   }
 
+  const handleClick = () => {
+    dispatch(
+      addProduct({ ...product, quantity, color, size })
+    )
+  }
+
   return (
     <Container>
       <Navbar />
@@ -160,12 +171,12 @@ const Product = () => {
             <Filter>
               <FilterTitle>Color</FilterTitle>
               {product.color?.map((c) => (
-                <FilterColor color={c} key={c}  />
+                <FilterColor color={c} key={c} onClick={() => setColor(c)} />
               ))}
             </Filter>
             <Filter>
               <FilterTitle>Size</FilterTitle>
-              <FilterSize >
+              <FilterSize onChange={(e) => setSize(e.target.value)}>
                 {product.size?.map((s) => (
                   <FilterSizeOption key={s}>{s}</FilterSizeOption>
                 ))}
@@ -178,7 +189,7 @@ const Product = () => {
               <Amount>{quantity}</Amount>
               <Add onClick={() => handleQuantity('inc')} />
             </AmountContainer>
-            <Button>ADD TO CART</Button>
+            <Button onClick={handleClick}>ADD TO CART</Button>
           </AddContainer>
         </InfoContainer>
       </Wrapper>
